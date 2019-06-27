ea08f45a5bce9ec5270da8a19ac7b65a98551240
xuwei-k <6b656e6a69@gmail.com>
2019-06-27 11:41:35 +0900

    Scala 2.13.0

--- support/scalajson/src/main/scala/sjsonnew/support/scalajson/unsafe/Parser.scala
+++ support/scalajson/src/main/scala/sjsonnew/support/scalajson/unsafe/Parser.scala
@@ -3,11 +3,13 @@ package support.scalajson.unsafe
 
 import shaded.scalajson.ast.unsafe._
 import scala.collection.mutable.ArrayBuffer
-import jawn.{ Facade, FContext, SupportParser }
+import org.typelevel.jawn.{ Facade, FContext, SupportParser }
 
 object Parser extends SupportParser[JValue] {
   implicit val facade: Facade[JValue] =
     new Facade[JValue] {
+      def jnum(s: CharSequence, decIndex: Int, expIndex: Int) = JNumber(s.toString)
+      def jstring(s: CharSequence) = JString(s.toString)
       def jnull() = JNull
       def jfalse() = JFalse
       def jtrue() = JTrue
@@ -16,6 +18,7 @@ object Parser extends SupportParser[JValue] {
       def jstring(s: String) = JString(s)
       def singleContext() = new FContext[JValue] {
         var value: JValue = _
+        def add(s: CharSequence) = add(s.toString)
         def add(s: String) = value = jstring(s)
         def add(v: JValue) = value = v
         def finish: JValue = value
@@ -23,6 +26,7 @@ object Parser extends SupportParser[JValue] {
       }
       def arrayContext() = new FContext[JValue] {
         private val vs = ArrayBuffer.empty[JValue]
+        def add(s: CharSequence) = add(s.toString)
         def add(s: String) = vs += jstring(s)
         def add(v: JValue) = vs += v
         def finish: JValue = JArray(vs.toArray)
@@ -32,6 +36,7 @@ object Parser extends SupportParser[JValue] {
         private var key: String = _
         private var vs = ArrayBuffer.empty[JField]
         private def andNullKey[A](t: => Unit): Unit = { t; key = null }
+        def add(s: CharSequence) = add(s.toString)
         def add(s: String) = if (key == null) key = s else andNullKey(vs += JField(key, jstring(s)))
         def add(v: JValue) = andNullKey(vs += JField(key, v))
         def finish: JValue = JObject(vs.toArray)
